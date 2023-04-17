580410942edce3580b706a02a6c93acb0007e3cb
Nitin052 <42588508+Nitin052@users.noreply.github.com>
2023-04-17 16:00:28 +0530

    Update terraform_test.tf

--- terraform_test.tf
+++ terraform_test.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myec2" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
   #key_name      = "test1"
-  #tags = {
-   # Name = "test12"
-  #}
+  tags = {
+   Name = "test12"
   }
+}
