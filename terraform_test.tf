2cb9a20809bfa9d4fe190d8c7ac6a415024a03ac
Nitin052 <42588508+Nitin052@users.noreply.github.com>
2023-04-17 15:03:15 +0530

    Update terraform_test.tf

--- terraform_test.tf
+++ terraform_test.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    organization = "test-terraform-cloud"
+
+    workspaces {
+      name = "terraform_test"
+    }
+  }
+}
+
 resource "aws_instance" "myec2" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
